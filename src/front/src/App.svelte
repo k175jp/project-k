df874a4cc913e3004bb9c199d64300786c74d325
kknmt <m20109@g.metro-cit.ac.jp>
2024-07-15 15:27:45 +0900

    [Fix] ヘッダーとメインコンテンツエリアを追加

--- src/front/src/App.svelte
+++ src/front/src/App.svelte
@@ -1,30 +1,18 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
+	import Header from './components/Header.svelte';
+	import SearchBar from './components/SearchBar.svelte';
+    import QuestionList from './components/QuestionList.svelte';
+  </script>
+  
+  <main>
+	<Header />
+	<SearchBar />
+	<QuestionList />
+  </main>
+  
+  <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	  font-family: Arial, Helvetica, sans-serif;
+	  padding: 20px;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  </style>
